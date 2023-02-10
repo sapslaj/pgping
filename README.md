412b19e342f6fe84252d9fef48413b44ceda514d
Justin Roberson <justin.roberson@tackle.io>
2023-02-10 11:20:15 -0500

    Make logs more machine-parsable, add application_name support, add short -h --help support

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Quick-n-dirty "ping" utility for testing connectivity to PostgreSQL databases
 usage: pgping [<flags>]
 
 Flags:
-      --help                     Show context-sensitive help (also try --help-long and --help-man).
+  -h, --help                     Show context-sensitive help (also try --help-long and --help-man).
   -c, --count=-1                 stop after N pings
   -i, --wait=1s                  wait time between sending each ping
   -t, --timeout=5s               timeout for connections to the DB
@@ -16,4 +16,5 @@ Flags:
       --pg-database=PG-DATABASE
       --pg-user=PG-USER
       --pg-password=PG-PASSWORD
+      --pg-app-name="pgping/0.1.0"
 ```
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net"
 	"strings"
 	"time"
@@ -23,26 +22,33 @@ var (
 	pgDatabase = kingpin.Flag("pg-database", "").Envar("PGDATABASE").String()
 	pgUser     = kingpin.Flag("pg-user", "").Envar("PGUSER").String()
 	pgPassword = kingpin.Flag("pg-password", "").Envar("PGPASSWORD").String()
+	pgAppName  = kingpin.Flag("pg-app-name", "").Default("pgping/0.1.0").Envar("PGAPPNAME").String()
 )
 
 func kv(key string, value any) string {
+	switch v := value.(type) {
+	case string:
+		value = fmt.Sprintf("%q", v)
+	}
 	return fmt.Sprintf("%s=%v", key, value)
 }
 
-func result(i int, start time.Time, msg string, kvs ...string) time.Duration {
+func result(i int, start time.Time, kvs ...string) time.Duration {
 	if kvs == nil {
 		kvs = make([]string, 2)
 	}
 	duration := time.Since(start)
-	kvs = append([]string{kv("time", duration)}, kvs...)
-	kvs = append([]string{kv("i", i)}, kvs...)
+	kvs = append(kvs, kv("i", i))
+	kvs = append(kvs, kv("duration", duration))
 	var format strings.Builder
-	format.WriteString(msg)
-	for _, label := range kvs {
-		format.WriteString(" ")
+	format.WriteString(time.Now().Format("2006-01-02T15:04:05.999Z") + " ")
+	for i, label := range kvs {
+		if i > 0 {
+			format.WriteString(" ")
+		}
 		format.WriteString(label)
 	}
-	log.Println(format.String())
+	fmt.Println(format.String())
 	return duration
 }
 
@@ -50,23 +56,24 @@ func ping(ctx context.Context, connConfig *pgx.ConnConfig, i int) time.Duration 
 	start := time.Now()
 	conn, err := pgx.ConnectConfig(ctx, connConfig)
 	if err != nil {
-		return result(i, start, "error connecting: ", kv("err", err))
+		return result(i, start, kv("msg", "error connecting"), kv("err", err))
 	}
 	rows, err := conn.Query(ctx, *query)
 	if err != nil {
-		return result(i, start, "error querying: ", kv("err", err))
+		return result(i, start, kv("msg", "error querying"), kv("err", err))
 	}
 	err = conn.Close(ctx)
 	if err != nil {
-		return result(i, start, "error closing: ", kv("err", err))
+		return result(i, start, kv("msg", "error closing"), kv("err", err))
 	}
 	if rows.Next() {
-		return result(i, start, fmt.Sprintf("%s: OK", connConfig.Host))
+		return result(i, start, kv("status", "OK"), kv("host", connConfig.Host))
 	}
-	return result(i, start, fmt.Sprintf("%s: FAIL", connConfig.Host))
+	return result(i, start, kv("status", "FAIL"), kv("host", connConfig.Host))
 }
 
 func main() {
+	kingpin.CommandLine.HelpFlag.Short('h')
 	kingpin.Parse()
 	ctx := context.Background()
 	var connString strings.Builder
@@ -90,6 +97,8 @@ func main() {
 		connString.WriteString("/")
 		connString.WriteString(*pgDatabase)
 	}
+	connString.WriteString("?application_name=")
+	connString.WriteString(*pgAppName)
 
 	connConfig, err := pgx.ParseConfig(connString.String())
 	if err != nil {
